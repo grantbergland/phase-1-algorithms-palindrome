9be77a6b0ecaf52e5d1d388f67adb52d84963b53
grantbergland <grantbergland@gmail.com>
2022-08-15 06:19:27 -0500

    commit tree 4ddaa3d578f0042cbd88fbe9c8bc3333b1dbc211

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+    var re = /[^A-Za-z0-9]/g;
+    word = word.toLowerCase().replace(re, '');
+    var len = word.length;
+    for (var i = 0; i < len/2; i++) {
+      if (word[i] !== word[len - 1 - i]) {
+          return false;
+      }
+    }
+    return true;
+   }
 
 /* 
   Add your pseudocode here
